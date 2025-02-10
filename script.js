a9d9ddd90a70a694974860f53faa50a3cc4005eb
alan-le-1 <atmle2002@gmail.com>
2025-02-09 21:42:20 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".search-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Searching for available stays...");
+});
+// JavaScript Document
