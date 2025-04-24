f1564c67a5897a4addc38369249333c2def4f175
alan-le-1 <atmle2002@gmail.com>
2025-04-23 20:39:33 -0400

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.querySelector(".search-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    alert("Searching for available stays...");
-});
-// JavaScript Document
